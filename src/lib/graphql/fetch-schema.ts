5173e6450a05a0c804f29e512c3eae20efe313e5
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2025-02-04 11:07:59 +0100

    fix fetch-schema

--- src/lib/graphql/fetch-schema.ts
+++ src/lib/graphql/fetch-schema.ts
@@ -3,8 +3,10 @@ import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const projectRoot = join(__dirname, "../../../");
+
 const packageJson = JSON.parse(
-  readFileSync(join(__dirname, "../../package.json"), "utf-8")
+  readFileSync(join(projectRoot, "package.json"), "utf-8")
 );
 
 const version = packageJson.saleor.schemaVersion;
@@ -12,4 +14,6 @@ const url = `https://raw.githubusercontent.com/saleor/saleor/${version}/saleor/g
 
 const response = await fetch(url);
 const schema = await response.text();
-writeFileSync("graphql/schema.graphql", schema);
+
+// Create the schema file in the project root's graphql directory
+writeFileSync(join(projectRoot, "src/lib/graphql/schema.graphql"), schema);
