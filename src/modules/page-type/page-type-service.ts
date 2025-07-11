38c74bedeeecc6249e7d96b33f15360235609804
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2025-07-11 14:02:43 +0200

    fix error

--- src/modules/page-type/page-type-service.ts
+++ src/modules/page-type/page-type-service.ts
@@ -1,7 +1,13 @@
 import { logger } from "../../lib/logger";
 import type { AttributeService } from "../attribute/attribute-service";
-import type { SimpleAttribute } from "../config/schema/attribute.schema";
-import type { PageTypeInput, PageTypeUpdateInput } from "../config/schema/schema";
+import type {
+  AttributeInput,
+  SimpleAttribute,
+} from "../config/schema/attribute.schema";
+import type {
+  PageTypeInput,
+  PageTypeUpdateInput,
+} from "../config/schema/schema";
 import type { PageTypeOperations } from "./repository";
 
 export class PageTypeService {
@@ -22,7 +28,10 @@ export class PageTypeService {
     return this.repository.createPageType({ name });
   }
 
-  private async filterOutAssignedAttributes(pageTypeId: string, attributeIds: string[]) {
+  private async filterOutAssignedAttributes(
+    pageTypeId: string,
+    attributeIds: string[]
+  ) {
     logger.debug("Checking for assigned attributes", {
       pageTypeId,
       attributeIds,
@@ -38,7 +47,9 @@ export class PageTypeService {
     const assignedAttributeIds = new Set(
       pageType.attributes.map((attr: { id: string }) => attr.id)
     );
-    const filteredIds = attributeIds.filter((id) => !assignedAttributeIds.has(id));
+    const filteredIds = attributeIds.filter(
+      (id) => !assignedAttributeIds.has(id)
+    );
 
     return filteredIds;
   }
@@ -75,11 +86,14 @@ export class PageTypeService {
         attributeInputs: attributesToCreate.map((a) => ({
           ...a,
           type: "PAGE_TYPE" as const,
-        })) as any,
+        })),
       });
 
       const attributeIds = attributes.map((attr) => attr.id);
-      const attributesToAssign = await this.filterOutAssignedAttributes(pageType.id, attributeIds);
+      const attributesToAssign = await this.filterOutAssignedAttributes(
+        pageType.id,
+        attributeIds
+      );
 
       if (attributesToAssign.length > 0) {
         logger.debug("Assigning attributes to page type", {
@@ -88,7 +102,10 @@ export class PageTypeService {
         });
 
         try {
-          await this.repository.assignAttributes(pageType.id, attributesToAssign);
+          await this.repository.assignAttributes(
+            pageType.id,
+            attributesToAssign
+          );
           logger.debug("Successfully assigned attributes to page type", {
             name: input.name,
           });
