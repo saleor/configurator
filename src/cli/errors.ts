8810d8ab47421c8561c6e43969445cdfb4a402e8
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2025-07-14 15:18:15 +0200

    remove unused errors

--- src/cli/errors.ts
+++ src/cli/errors.ts
@@ -7,17 +7,3 @@ export class CliArgumentError extends CliError {
     super(message, "CLI_ARGUMENT_ERROR");
   }
 }
-
-export class CliValidationError extends CliError {
-  constructor(message: string) {
-    super(message, "CLI_VALIDATION_ERROR");
-  }
-}
-
-class CliFileError extends CliError {}
-
-export class CliFileNotFoundError extends CliFileError {
-  constructor(message: string) {
-    super(message, "CLI_FILE_NOT_FOUND_ERROR");
-  }
-}
